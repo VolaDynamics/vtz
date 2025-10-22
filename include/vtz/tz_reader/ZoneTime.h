f25e98027abae0567ea74a0d6c5002e510e2f25b
Alecto Irene Perez <alecto.perez@voladynamics.com>
2025-10-22 10:46:58 -0400

    add vtz::ZoneTime - holds stdoff and walloff together

---
+++ include/vtz/tz_reader/ZoneTime.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <vtz/bit.h>
+#include <vtz/tz_reader/FromUTC.h>
+#include <vtz/tz_reader/RuleSave.h>
+
+namespace vtz {
+    /// Holds the stdoff and walloff for a zone at a particular point in time
+    struct alignas( 8 ) ZoneTime {
+        FromUTC stdoff;  ///< Zone stdoff (non-DST offset)
+        FromUTC walloff; ///< Actual offset in this state
+
+        /// Return the save implied by the zone
+        constexpr RuleSave save() const noexcept { return walloff.off - stdoff.off; }
+
+        bool operator==( ZoneTime const& rhs ) const noexcept {
+            return B8( *this ) == B8( rhs );
+        }
+
+        bool operator!=( ZoneTime const& rhs ) const noexcept {
+            return B8( *this ) != B8( rhs );
+        }
+    };
+} // namespace vtz
