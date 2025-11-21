e8c2931467fb354ff3f219e6db91a12874762370
Alecto Irene Perez <alecto.perez@voladynamics.com>
2025-11-21 18:09:32 -0500

    more compatibility changes

--- include/vtz/date.h
+++ include/vtz/date.h
@@ -3,6 +3,29 @@
 #include <vtz/civil.h>
 
 
+namespace vtz {
+    using std::chrono::hours;
+    using std::chrono::minutes;
+    using std::chrono::nanoseconds;
+    using std::chrono::seconds;
+    using std::chrono::floor;
+
+    using days = std::chrono::duration<i32, std::ratio<86400>>;
+    using std::chrono::system_clock;
+    using std::chrono::time_point;
+
+    template<class Duration>
+    using sys_time    = time_point<system_clock, Duration>;
+    using sys_seconds = sys_time<seconds>;
+    using sys_days    = sys_time<days>;
+
+    struct local_t {};
+    template<class Duration>
+    using local_time = std::chrono::time_point<local_t, Duration>;
+
+    using local_seconds = local_time<seconds>;
+    using local_days    = local_time<days>;
+} // namespace vtz
 #ifdef VTZ_DATE_COMPAT
 namespace date = vtz;
 #endif
--- include/vtz/tz.h
+++ include/vtz/tz.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "vtz/civil.h"
+#include <vtz/date.h>
 #include <chrono>
 #include <climits>
 #include <cstdint>
@@ -49,28 +49,6 @@ namespace vtz::detail {
 
 
 namespace vtz {
-
-    using std::chrono::hours;
-    using std::chrono::minutes;
-    using std::chrono::nanoseconds;
-    using std::chrono::seconds;
-
-    using days = std::chrono::duration<i32, std::ratio<86400>>;
-    using std::chrono::system_clock;
-    using std::chrono::time_point;
-
-    template<class Duration>
-    using sys_time    = time_point<system_clock, Duration>;
-    using sys_seconds = sys_time<seconds>;
-    using sys_days    = sys_time<days>;
-
-    struct local_t {};
-    template<class Duration>
-    using local_time = std::chrono::time_point<local_t, Duration>;
-
-    using local_seconds = local_time<seconds>;
-    using local_days    = local_time<days>;
-
 
     struct sys_info {
         sys_seconds begin;
