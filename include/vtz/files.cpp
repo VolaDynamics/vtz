90c83ee92425722f9198a040e5a6009b29a77f37
Alecto Irene Perez <alecto.perez@voladynamics.com>
2026-01-08 10:30:56 -0500

    chg join_path: if file is absolute, don't append to prefix

--- include/vtz/files.cpp
+++ include/vtz/files.cpp
@@ -33,11 +33,30 @@ namespace vtz {
 #else
         constexpr char FILE_SEP = '/';
 #endif
+
+
+        /// Returns true if the given path is absolute
+        ///
+        /// - '' -> false
+        /// - 'C:/...' -> true (on windows) (returns true for other drive
+        /// letters)
+        /// - /... -> true (any path starting with a path separator is true)
+        bool is_absolute( string_view file ) noexcept {
+            if( file.empty() ) return false;
+
+#if _WIN32
+            if( file.size() >= 3 && file[1] == ':' && is_file_sep( file[2] ) )
+                return true;
+#endif
+
+            return is_file_sep( file[0] );
+        }
     } // namespace
 
+
     std::string join_path( string_view prefix, string_view file ) {
-        // If the prefix is empty, just return the file
-        if( prefix.empty() ) return std::string( file );
+        // If the prefix is empty or the file is absolute, just return the file
+        if( prefix.empty() || is_absolute( file ) ) return std::string( file );
 
         // If it already ends in a file separator, just add the file
         if( is_file_sep( prefix.back() ) )
