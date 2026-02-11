2121868d52e7aad05e4f8a09944b9108fb5eb4f9
Alecto Irene Perez <alecto.perez@voladynamics.com>
2026-02-11 12:20:25 -0500

    fix use of __has_builtin on msvc

--- include/vtz/bit.h
+++ include/vtz/bit.h
@@ -4,21 +4,32 @@
 #include <cstdint>
 #include <type_traits>
 
-#if __has_builtin( __builtin_memcpy )
-    #define _vtz_memcpy __builtin_memcpy
-#else
+#if _MSC_VER
     #include <cstring>
     #define _vtz_memcpy memcpy
+#else
+    #if __has_builtin( __builtin_memcpy )
+        #define _vtz_memcpy __builtin_memcpy
+    #else
+        #include <cstring>
+        #define _vtz_memcpy memcpy
+    #endif
 #endif
 
 #if _MSC_VER
     #define VTZ_INLINE            __forceinline
     #define VTZ_IS_LIKELY( expr ) bool( expr )
     #define VTZ_UNREACHABLE() __assume(0)
+    #if _MSVC_LANG >= 202002L
+        #define VTZ_LIKELY [[likely]]
+    #else
+        #define VTZ_LIKELY
+    #endif
 #else
     #define VTZ_INLINE            [[gnu::always_inline]] inline
     #define VTZ_IS_LIKELY( expr ) __builtin_expect( bool( expr ), 1 )
     #define VTZ_UNREACHABLE() __builtin_unreachable()
+    #define VTZ_LIKELY [[likely]]
 #endif
 
 namespace vtz {
@@ -57,10 +68,14 @@ namespace vtz {
     ///
     /// Uses __builtin_clzll if available
     constexpr int _blog2( u64 x ) noexcept {
-#if __has_builtin( __builtin_clzll )
-        return 63 - __builtin_clzll( x );
-#else
+#if _MSC_VER
         return _blog2_fallback( x );
+#else
+    #if __has_builtin( __builtin_clzll )
+        return 63 - __builtin_clzll( x );
+    #else
+        return _blog2_fallback( x );
+    #endif
 #endif
     }
 
