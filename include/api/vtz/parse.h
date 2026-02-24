410e7c2ce660e0b427ab502232cb172a379a201c
Alecto Irene Perez <alecto.perez@voladynamics.com>
2026-02-24 17:48:26 -0500

    implement vtz::parse<T> for arbitrary duration

--- include/api/vtz/parse.h
+++ include/api/vtz/parse.h
@@ -4,10 +4,12 @@
 #include <cstddef>
 #include <string_view>
 
+#include <type_traits>
 #include <vtz/impl/chrono_types.h>
 #include <vtz/impl/export.h>
 #include <vtz/types.h>
 
+#include <vtz/impl/tz_impl.h>
 
 namespace vtz {
     using std::string_view;
@@ -133,4 +135,113 @@ namespace vtz {
     VTZ_EXPORT parse_precise_result parse_precise(
         string_view fmt, string_view time_str );
 
+    template<class Dur>
+    sys_time<Dur> parse( string_view fmt, string_view time_str ) {
+        using rep        = typename Dur::rep;
+        using period     = typename Dur::period;
+        constexpr auto n = period::num;
+        constexpr auto d = period::den;
+
+        using _tp = sys_time<Dur>;
+
+        using std::chrono::floor;
+        if constexpr( !std::is_floating_point<rep>() )
+        {
+            if constexpr( d == 1 )
+            {
+                if constexpr( n % 86400 == 0 ) // result is a multiple of a day
+                {
+                    if constexpr( n == 86400 )
+                    {
+                        // result is exactly a day
+                        return _tp{ days( parse_date_d( fmt, time_str ) ) };
+                    }
+                    else
+                    {
+                        // result is multiple of a day
+                        return _tp{ floor<Dur>(
+                            days( parse_date_d( fmt, time_str ) ) ) };
+                    }
+                }
+                else
+                {
+                    if constexpr( n == 1 )
+                    {
+                        // result is exactly a second
+                        return _tp{ seconds( parse_time_s( fmt, time_str ) ) };
+                    }
+                    else
+                    {
+                        // result is a multiple of a second
+                        return _tp{ floor<Dur>(
+                            seconds( parse_time_s( fmt, time_str ) ) ) };
+                    }
+                }
+            }
+            else
+            {
+                if constexpr( 1000000000 % d == 0 )
+                {
+                    // result can be expressed as a whole number of nanoseconds
+                    // So, we put the result in terms of (1/d)
+
+                    auto parts = parse_precise( fmt, time_str );
+
+                    constexpr u32 nanos_div = u32( 1000000000u / d );
+                    // If a whole number of nanoseconds fit into the
+                    // denominator, we want to put both seconds and nanoseconds
+                    // in terms of (1/den)
+                    int64_t t = parts.seconds * d         // seconds -> 1/d
+                                + parts.nanos / nanos_div // nanos -> 1/d
+                        ;
+                    if constexpr( n == 1 )
+                    {
+                        // Duration is in terms of 1/d seconds, which is t
+                        return _tp{ Dur( t ) };
+                    }
+                    else
+                    {
+                        // Duration is in terms of n/d, so we need to divide out
+                        // n
+                        return _tp{ Dur( math::div_floor<n>( t ) ) };
+                    }
+                }
+                else
+                {
+                    // result can NOT be expressed as a whole number of
+                    // nanoseconds (or a whole number of seconds, for that
+                    // matter)
+                    //
+                    // Best thing to do is (1) convert to nanoseconds
+
+                    return _tp{ floor<Dur>(
+                        nanoseconds( parse_time_ns( fmt, time_str ) ) ) };
+                }
+            }
+        }
+        else
+        {
+            // rep is floating point
+
+            auto parts = parse_precise( fmt, time_str );
+
+            constexpr rep ns_div = 1000000000 / rep( d );
+            // If a whole number of nanoseconds fit into the
+            // denominator, we want to put both seconds and nanoseconds
+            // in terms of (1/den)
+            rep result = rep( parts.seconds ) * rep( d ) // seconds -> 1/d
+                         + rep( parts.nanos ) / ns_div   // nanos -> 1/d
+                ;
+            if constexpr( n == 1 )
+            {
+                // Duration is in terms of 1/d seconds, which is t
+                return _tp{ Dur( result ) };
+            }
+            else
+            {
+                // Divide out the numerator
+                return _tp{ Dur( result / rep( n ) ) };
+            }
+        }
+    }
 } // namespace vtz
