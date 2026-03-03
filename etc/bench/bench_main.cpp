902c6eea556654e81ee3b8c13bd9fe16fab3840a
Alecto Irene Perez <alecto.perez@voladynamics.com>
2026-03-03 17:26:27 -0500

    disable address space layout randomization for benchmarks
    Reduces noice in benchmarks.

--- etc/bench/bench_main.cpp
+++ etc/bench/bench_main.cpp
@@ -4,6 +4,11 @@
 #include <vtz/tz.h>
 
 int main( int argc, char** argv ) {
+    // Disable address space layout randomization to reduce noise in benchmarks
+    //
+    // See: https://google.github.io/benchmark/reducing_variance.html
+    benchmark::MaybeReenterWithoutASLR(argc, argv);
+
     try
     {
         benchmark::Initialize( &argc, argv );
