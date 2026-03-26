33e10dc719f48f02138a245d19b258460670104e
Alecto Irene Perez <alecto.perez@voladynamics.com>
2026-03-26 12:49:32 -0400

    fix compiler error on gcc9 - gcc9 lacks `__has_builtin`

--- include/api/vtz/impl/macros.h
+++ include/api/vtz/impl/macros.h
@@ -1,11 +1,18 @@
 #pragma once
 
+// For older compilers:
+// If we don't have __has_builtin, assume that the builtin
+// is missing/not present
+#ifndef __has_builtin
+    #define __has_builtin( ... ) 0
+#endif
 
 #if _MSC_VER
     #include <cstring>
     #define _vtz_memcpy memcpy
 #else
-    #if __has_builtin( __builtin_memcpy )
+    // Compilers conforming to __GNUC__ should have __builtin_memcpy
+    #if defined( __GNUC__ ) || __has_builtin( __builtin_memcpy )
         #define _vtz_memcpy __builtin_memcpy
     #else
         #include <cstring>
